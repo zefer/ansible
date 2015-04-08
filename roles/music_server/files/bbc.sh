549001212b8377aa5cc47a1231841d13bdd970f8
Joe Roberts <joe@zefer.co.uk>
2015-04-08 13:57:26 +0100

    more new bbc feeds, super uncool BBC

--- roles/music_server/files/bbc.sh
+++ roles/music_server/files/bbc.sh
@@ -4,12 +4,12 @@ playlistdir=/var/lib/mpd/music/radio
 mkdir -p $playlistdir
 
 declare -A radios
-radios["BBC Radio 1"]="http://open.live.bbc.co.uk/mediaselector/5/select/mediaset/http-icy-aac-lc-a/vpid/bbc_radio_one/format/pls.pls"
-radios["BBC Radio 2"]="http://open.live.bbc.co.uk/mediaselector/5/select/mediaset/http-icy-aac-lc-a/vpid/bbc_radio_two/format/pls.pls"
-radios["BBC Radio 3"]="http://open.live.bbc.co.uk/mediaselector/5/select/mediaset/http-icy-aac-lc-a/vpid/bbc_radio_three/format/pls.pls"
-radios["BBC Radio 4"]="http://open.live.bbc.co.uk/mediaselector/5/select/mediaset/http-icy-aac-lc-a/vpid/bbc_radio_fourfm/format/pls.pls"
-radios["BBC Radio 4 Extra"]="http://open.live.bbc.co.uk/mediaselector/5/select/mediaset/http-icy-aac-lc-a/vpid/bbc_radio_four_extra/format/pls.pls"
-radios["BBC 6 Music"]="http://open.live.bbc.co.uk/mediaselector/5/select/mediaset/http-icy-aac-lc-a/vpid/bbc_6music/format/pls.pls"
+radios["BBC Radio 1"]="http://www.radiofeeds.co.uk/bbcradio1.pls"
+radios["BBC Radio 2"]="http://www.radiofeeds.co.uk/bbcradio2.pls"
+radios["BBC Radio 3"]="http://www.radiofeeds.co.uk/bbcradio3.pls"
+radios["BBC Radio 4"]="http://www.radiofeeds.co.uk/bbcradio4fm.pls"
+radios["BBC Radio 4 Extra"]="http://www.radiofeeds.co.uk/bbcradio4extra.pls"
+radios["BBC 6 Music"]="http://www.radiofeeds.co.uk/bbc6music.pls"
 
 for k in "${!radios[@]}"
 do
